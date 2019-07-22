c77478e2660557b559521984c9288a15e6b1314a
MyName <48720376+benj555@users.noreply.github.com>
2019-07-22 22:31:05 +0300

    changes to db object

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-convex-75268',
-    user : 'postgres',   //aneagoie
-    password : 'test',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
